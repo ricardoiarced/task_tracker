159bed5e2d6de86af6bb32abb133d39a2cde46ee
Ricardo Arce <ricardoiarced@gmail.com>
2025-05-24 23:30:57 -0700

    feat: implement update_task and add its test

--- task_tracker.py
+++ task_tracker.py
@@ -9,5 +9,10 @@ def add_task(db: dict[str, dict], description: str) -> None:
         "description": description,
         "status": "todo",
         "created-at": today,
-        "updated-at": today,
+        "updated-at": today
     }
+
+def update_task(db: dict[str, dict], id: str, description: str) -> None:
+    today = datetime.today().strftime("%Y/%m/%d %H:%M:%S")
+    db[id]["description"] = description
+    db[id]["updated-at"] = today

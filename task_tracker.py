eb084d2c00b61d4c244a1ca118f511ffc889fbd2
Ricardo Irvin Arce Diaz <88550263+ricardoiarced@users.noreply.github.com>
2025-05-29 21:57:06 -0700

    feat: add list_tasks and their tests

--- task_tracker.py
+++ task_tracker.py
@@ -1,6 +1,8 @@
 import argparse
 import json, os
 from datetime import datetime
+from typing import Literal
+from tabulate import tabulate
 
 DATE_FMT = "%Y/%m/%d %H:%M:%S"
 
@@ -19,13 +21,31 @@ def update_task(db: dict[str, dict], id: str, description: str) -> None:
     db[id]["updated-at"] = datetime.now().strftime(DATE_FMT)
 
 def delete_task(db: dict[str, dict], id: str) -> None:
-    
     del db[id]
 
 def mark_task_in_progress(db: dict[str, dict], id: str) -> None:
-    db[id]["updated-at"] = datetime.now().strftime(DATE_FMT)
     db[id]["status"] = "in-progress"
+    db[id]["updated-at"] = datetime.now().strftime(DATE_FMT)
 
 def mark_task_done(db: dict[str, dict], id: str) -> None:
-    db[id]["updated-at"] = datetime.now().strftime(DATE_FMT)
     db[id]["status"] = "done"
+    db[id]["updated-at"] = datetime.now().strftime(DATE_FMT)
+
+def list_tasks(db: dict[str, dict], status: Literal["all", "todo", "in-progress", "done"] = "all",
+               ) -> None:
+    rows = []
+
+    for task_id, props in sorted(db.items(), key=lambda t: int(t[0])):
+        if status == "all" or props["status"] == status:
+            rows.append({
+                "id": task_id,
+                "description": props["description"],
+                "status": props["status"],
+                "created-at": props["created-at"],
+                "updated-at": props["updated-at"],
+            })
+
+            if rows:
+                print(tabulate(rows, headers="keys", tablefmt="rounded_grid"))
+            else:
+                print("Nothing to display")
